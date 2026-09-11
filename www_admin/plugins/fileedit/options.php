61e2b4b14c05b85ca2a6b71756f9d10fe28b2f11
Martin Fiedler <keyj@emphy.de>
2026-09-11 23:43:00 +0200

    fileedit: more comprehensive list of editable files
    Instead of only a few hardcoded filenames, the file list now does a
    more thorough scan for customizable text files. This helps with
    scenarios where the styling of the visitor webpage happens in a
    subdirectory, for example.

--- www_admin/plugins/fileedit/options.php
+++ www_admin/plugins/fileedit/options.php
@@ -1,6 +1,13 @@
 <?php
 if (!defined("PLUGINOPTIONS")) exit();
 
+$TEXT_EXTENSIONS = array("txt", "md", "rst", "htm", "html", "xhtml", "xml", "js", "mjs", "json", "css", "svg", "csv", "ics");
+function istextfile($fn)
+{
+  global $TEXT_EXTENSIONS;
+  return in_array(strtolower(pathinfo($fn, PATHINFO_EXTENSION)), $TEXT_EXTENSIONS);
+}
+
 if (isset($_GET["file"]))
 {
   $filename = $_GET["file"];
@@ -26,10 +33,44 @@ if (isset($_GET["file"]))
 }
 else
 {
+  // get list of files (with absolute paths, at this point)
+  $files = array(
+    ADMIN_DIR . "/results_header.txt",
+    WWW_DIR . "/template.html"
+  );
+  array_push($files, ... glob(ADMIN_DIR . "/slideviewer/custom.*"));
+  array_push($files, ... glob(WWW_DIR . "/*/*"));  // subdirectories of www_party
+  $files = array_filter($files, 'istextfile');
+  sort($files);
+
+  // find common prefix (for the UI)
+  $a = $files[0];
+  $b = $files[count($files) - 1];
+  $prefix = 0;
+  $len = min(strlen($a), strlen($b));
+  for ($i = 0;  $i < $len;  $i++)
+  {
+    if ($a[$i] != $b[$i]) { break; }
+    if ($a[$i] == "/") { $prefix = $i + 1; }
+  }
+
+  // render UI
   echo "<p>Select file to edit:</p><ul>\n";
-  echo "<li><a href=\"pluginoptions.php?plugin=fileedit&file=../www_party/template.html\">visitor website template</a></li>\n";
-  echo "<li><a href=\"pluginoptions.php?plugin=fileedit&file=slideviewer/custom.css\">slideviewer CSS</a></li>\n";
-  echo "<li><a href=\"pluginoptions.php?plugin=fileedit&file=results_header.txt\">results file header</a></li>\n";
+  $root = explode("/", ADMIN_DIR);
+  foreach ($files as $fn)
+  {
+    // get relative path
+    $path = explode("/", $fn);
+    $len = min(count($root), count($path));
+    $common = 0;
+    for ($i = 0;  $i < $len;  $i++)
+    {
+      if ($path[$i] == $root[$i]) { $common = $i + 1; } else { break; }
+    }
+    $rel = _html(str_repeat("../", count($root) - $common) . implode("/", array_slice($path, $common)));
+    $disp = _html(substr($fn, $prefix));
+    echo "<li><a href=\"pluginoptions.php?plugin=fileedit&file=$rel\">$disp</a></li>\n";
+  }
   echo "</ul>\n";
 }
 
